eab494721f2b279ebb2341761d6f55a1344389c7
Omar Ali Al-Gammal <omar.elgammal2810@gmail.com>
2024-10-05 14:34:38 +0300

    variables.tf

--- modules/Ec2/variables.tf
+++ modules/Ec2/variables.tf
@@ -15,5 +15,5 @@ variable "ec2_private_subnet_id" {
 
 variable "ec2_html" {
   type    = list(string)
-  default = ["Omar Ali Al-Gammal   Avail.Zone1", "Omar Ali Al-Gammal    Avail.Zone2"] 
+  default = ["Omar Ali Al-Gammal    Avail.Zone1", "Omar Ali Al-Gammal    Avail.Zone2"] 
 }
